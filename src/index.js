8678ab565a0d97fb61408db6b439d3ece01154db
Juan Hüttemann <juanfhuttemann@gmail.com>
2020-04-18 02:02:40 -0400

    add new icons

--- src/index.js
+++ src/index.js
@@ -1,15 +1,62 @@
-import { library, dom } from '@fortawesome/fontawesome-svg-core'
+import { library, dom } from "@fortawesome/fontawesome-svg-core";
 
-import { faSearch } from '@fortawesome/free-solid-svg-icons'
-import { faFileArchive } from '@fortawesome/free-solid-svg-icons'
-import { faDownload } from '@fortawesome/free-solid-svg-icons'
-import { faGithub } from '@fortawesome/free-brands-svg-icons'
+import {
+  faHome,
+  faLocationArrow,
+  faMapMarkerAlt,
+  faEdit,
+  faHandPointDown,
+  faThumbsUp,
+  faThumbsDown,
+  faPeopleCarry,
+  faHandHoldingHeart,
+  faUpload,
+  faCoffee,
+  faMedkit,
+  faShower,
+  faUser,
+  faLock,
+  faChild,
+  faPrescriptionBottleAlt,
+  faPumpSoap,
+  faUtensils,
+} from "@fortawesome/free-solid-svg-icons";
+
+import {
+  faGithub,
+  faTwitter,
+  faInstagram,
+  faFacebook,
+  faYoutube,
+  faWhatsapp,
+} from "@fortawesome/free-brands-svg-icons";
 
 library.add(
-  faSearch,
-  faFileArchive,
-  faDownload,
-  faGithub
-)
+  faHome,
+  faLocationArrow,
+  faMapMarkerAlt,
+  faEdit,
+  faHandPointDown,
+  faThumbsUp,
+  faThumbsDown,
+  faPeopleCarry,
+  faHandHoldingHeart,
+  faUpload,
+  faCoffee,
+  faMedkit,
+  faShower,
+  faChild,
+  faUser,
+  faLock,
+  faGithub,
+  faTwitter,
+  faInstagram,
+  faFacebook,
+  faYoutube,
+  faPrescriptionBottleAlt,
+  faUtensils,
+  faPumpSoap,
+  faWhatsapp
+);
 
-dom.watch()
+dom.watch();
